41c629c5f21ad96036ccebe385cf20eb10bcf97f
emb <emb@catfact.net>
2019-07-25 00:38:59 -0400

    add trivial processor, crash

--- Examples_Android/SuperpoweredPlayer/soniccloud/src/main/cpp/ScDsp.cpp
+++ Examples_Android/SuperpoweredPlayer/soniccloud/src/main/cpp/ScDsp.cpp
@@ -7,10 +7,13 @@ void ScDsp::setAudiogram(int numPoints, float *frequencies, float *leftValues, f
 }
 
 void ScDsp::setParameter(int index, float value) {
-
+    if (index == 0) {
+        gain.setGain(value);
+    }
     __android_log_print(ANDROID_LOG_ERROR, "SonicCloudSDK", "setParameter %f", value);
 }
 
 void ScDsp::process(int numSamples, float *inL, float *inR, float *outL, float *outR) {
+    gain.process(numSamples, inL, inR, outL, outR);
     __android_log_print(ANDROID_LOG_ERROR, "SonicCloudSDK", "process %d", numSamples);
 }
